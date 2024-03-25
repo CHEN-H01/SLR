c48e014c3d994e91fe49beac5b254ad68994c544
CHEN-H01 <1162737898@qq.com>
2024-03-25 15:23:18 +0800

    ch1

--- SLR/SLR/ContentView.swift
+++ SLR/SLR/ContentView.swift
@@ -1,16 +1,17 @@
 import Foundation
 import SwiftUI
-import AVKit
-//import AVFoundation
+import PhotosUI
+import UniformTypeIdentifiers
+//import Alamofire
 
 struct RoundedAndShadowProButtonStyle: ButtonStyle {
     @Environment(\.controlSize) var controlSize
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .frame(width:150, height: 30, alignment: .center)
+            .frame(width:150, height: 20, alignment: .center)
             .foregroundColor(.white)
             .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
-            .font(.headline)
+            .font(.body)
             .background(
                 RoundedRectangle(cornerRadius: 10)
                     .foregroundColor(configuration.role == .destructive ? .red : .blue)
@@ -39,19 +40,31 @@ extension ButtonStyle where Self == RoundedAndShadowProButtonStyle {
 
 struct ContentView: View {
     @State private var showCamera = false
+    @State private var showPhotoPicker = false
+
     var body: some View {
         ZStack {
-            
-            HStack(spacing: 10) {
+            VStack(spacing: 20) {
+                Button(action: { video2text("Translation") }, label: { label0 })
+                    .buttonStyle(.roundedAndShadowPro)
                 
-                Button(action: { open_camera_action("Open camera") }, label: { label1 })
-                .buttonStyle(.roundedAndShadowPro)
-                .sheet(isPresented: $showCamera) {
-                    CameraViewAdapter()
+                HStack(spacing: 10){
+                    Button(action: { open_camera_action("Open Camera") }, label: { label1 })
+                        .buttonStyle(.roundedAndShadowPro)
+                        .sheet(isPresented: $showCamera) {
+                            CameraViewAdapter()
+                        }
+                    
+                    Button(action: { upload_action("Upload Video") }, label: { label2 })
+                        .buttonStyle(.roundedAndShadowPro)
+                        .sheet(isPresented: $showPhotoPicker) {
+                            //                    PhotoPicker(selectionLimit: 1, filter: .videos) { results in
+                            //                        if let result = results.first {
+                            //                            handlePickedVideo(result)
+                            //                        }
+                            //                    }
+                        }
                 }
-                
-                Button(action: { upload_action("Upload video") }, label: { label2 })
-                .buttonStyle(.roundedAndShadowPro)
                 
             }
         }
@@ -66,23 +79,58 @@ struct ContentView: View {
         )
     }
     
+    func video2text(_ text: String) {
+        print(text)
+    }
+    
     func open_camera_action(_ text: String) {
         showCamera = true
         print(text)
     }
     
     func upload_action(_ text: String) {
+        showPhotoPicker = true
         print(text)
     }
+
+
+//    func handlePickedVideo(_ result: PHPickerResult) {
+//        result.itemProvider.loadFileRepresentation(forTypeIdentifier: UTType.movie.identifier) { url, error in
+//            guard let url = url, error == nil else { return }
+//
+//            // Update UI if needed
+//            DispatchQueue.main.async {
+//                self.videoURL = url
+//                // Now you can upload the video to your server
+//                uploadVideoToServer(videoURL: url)
+//            }
+//        }
+//    }
+
+//    func uploadVideoToServer(videoURL: URL) {
+//        // Specify your server upload URL and parameters
+//        let uploadURL = "https://yourserver.com/upload"
+//
+//        AF.upload(multipartFormData: { multipartFormData in
+//            multipartFormData.append(videoURL, withName: "video", fileName: videoURL.lastPathComponent, mimeType: "video/mp4")
+//        }, to: uploadURL).response { response in
+//            switch response.result {
+//            case .success(let responseData):
+//                // Handle success
+//                print("Video uploaded successfully: \(String(describing: responseData))")
+//            case .failure(let error):
+//                // Handle error
+//                print("Error uploading video: \(error.localizedDescription)")
+//            }
+//        }
+//    }
 }
     
+let label0 = Label("Translation", systemImage: "rectangle.and.pencil.and.ellipsis")
 let label1 = Label("Open Camera", systemImage: "camera.circle.fill")
 let label2 = Label("Upload Video", systemImage: "square.and.arrow.up.circle.fill")
 
 
-let videoURL = URL(string: "https://example.com/video.mp4")
-let player = AVPlayer(url: videoURL!)
-let playerViewController = AVPlayerViewController()
 
 
 
